74b71740866af9fa21a3176556fbd7ccc2fe029d
Nuutti <nuutti.kyllonen.2006@gmail.com>
2026-05-08 12:55:02 +0300

    add history list, because we need that agents know recently text, that agent will work

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ print("Welcome")
 
 
 def loop():
+    history = []
     while True:
         message = input("You: ")
         response = agent(message)
+        history.append({"role": "user", "parts": message})
+        history.append({"role":  "model", "parts": response})
+
         print(response)
 
 
